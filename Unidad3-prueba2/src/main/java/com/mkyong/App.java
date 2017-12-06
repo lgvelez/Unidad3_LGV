c20f93ea18f4cbcfec566e4f822d5b2aedb815f0
luis.velez <luis.velez@DESARROLLO-LGV.LINEADATASCAN.local>
2017-12-05 20:32:25 -0500

    manejo de relaciones de datos

--- Unidad3-prueba2/src/main/java/com/mkyong/App.java
+++ Unidad3-prueba2/src/main/java/com/mkyong/App.java
@@ -1,9 +1,17 @@
 package com.mkyong;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.hibernate.Session;
+
 import com.mkyong.util.HibernateUtil;
 import com.mkyong.user.DBUser;
+import com.mkyong.stock.Category;
+import com.mkyong.stock.Stock;
+import com.mkyong.stock.StockDailyRecord;
+
 
 public class App {
 	public static void main(String[] args) {
@@ -11,14 +19,51 @@ public class App {
 		Session session = HibernateUtil.getSessionFactory().openSession();
 
 		session.beginTransaction();
-		DBUser user = new DBUser();
+//
+//		DBUser user = new DBUser();
+//		user.setUserId(100);
+//		user.setUsername("superman");
+//		user.setCreatedBy("system");
+//		user.setCreatedDate(new Date());
+//		session.saveOrUpdate(user);
+//
+//		user = new DBUser();
+//		user.setUserId(101);
+//		user.setUsername("batman");
+//		user.setCreatedBy("superman");
+//		user.setCreatedDate(new Date());
+//		session.saveOrUpdate(user);
 
-		user.setUserId(100);
-		user.setUsername("superman");
-		user.setCreatedBy("system");
-		user.setCreatedDate(new Date());
+		Stock stock = new Stock();
+		stock.setStockCode("7052");
+		stock.setStockName("PADINI");
+		//session.saveOrUpdate(stock);
 
-		session.save(user);
+		StockDailyRecord stockDailyRecords = new StockDailyRecord();
+		stockDailyRecords.setPriceOpen(new Float("1.2"));
+		stockDailyRecords.setPriceClose(new Float("1.1"));
+		stockDailyRecords.setPriceChange(new Float("10.0"));
+		stockDailyRecords.setVolume(3000000L);
+		stockDailyRecords.setDate(new Date());
+
+		stockDailyRecords.setStock(stock);
+		stock.getStockDailyRecords().add(stockDailyRecords);
+
+		
+
+	    Category category1 = new Category("CONSUMER", "CONSUMER COMPANY");
+        Category category2 = new Category("INVESTMENT", "INVESTMENT COMPANY");
+
+        Set<Category> categories = new HashSet<Category>();
+        categories.add(category1);
+        categories.add(category2);
+
+        stock.setCategories(categories);
+
+        session.saveOrUpdate(stock);
+        
+        session.saveOrUpdate(stockDailyRecords);
+		
 		session.getTransaction().commit();
 	}
 }
